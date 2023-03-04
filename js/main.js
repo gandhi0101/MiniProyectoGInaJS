a8d31596d3cbc96faf95cd45577eab7a464df89d
gandhi0101 <117114451+gandhi0101@users.noreply.github.com>
2023-03-03 18:48:04 -0600

    reparamos los erroresdesdel index y el ingresa alias

--- js/main.js
+++ js/main.js
@@ -1,22 +1,24 @@
-
+const persona = new User();
 if (window.location.pathname.split('/').pop() === 'index.html') {
-    const persona = new User();
+    
     const inicio = new Fondo("../media/assets/Pantalla-presentacion/fondo-presentacion.png", 0, 0, 800, 600, 'mi-canvas');
-    const empezar = new Botones('', 400, 300, 300, 150, "../media/assets/Pantalla-presentacion/boton-inicio.png")
+    const empezar = new Botones('', 400, 400, 300, 150, "../media/assets/Pantalla-presentacion/boton-inicio.png")
     const exit = new Botones('', 60, 50, 300, 150, "../media/assets/Pantalla-principal/exit.png")
-    const logo = new Fondo(("../media/assets/Pantalla-presentacion/logo.png", 0, 0, 800, 600, 'mi-canvas');)
+    const logo = new Botones('',  390, 220, 200, 150,"../media/assets/Pantalla-presentacion/logo.png", 200, 200, 800, 600, 'mi-canvas');
     // hay que tener cuidado de que se cargue primero el fondo antes que cualquier cosa porque de lo contrario se manda al fondo el resto de elenentos
     // hay que tener cuidado de que se cargue primero el fondo antes que cualquier cosa porque de lo contrario se manda al fondo el resto de elenentos
-   
 
 
 
 
     inicio.dibujaFondo();
-    setTimeout(function () {
+
+    window.addEventListener('load', function () {
+        logo.dibujarImg();
         empezar.dibujarImg();
         exit.dibujarImg();
-    }, 150);
+    });
+
     exit.botonPresionado('../media/sounds/hasta_luegor.mp3', '../ingresarAlias.html')
 
     empezar.botonPresionado('../media/sounds/vamos_a_empezar.mp3', "../game.html")
